ab17d3c7db776e4e52c8539eafe0e0d307b06688
Mayank Aggarwal <97796657+mayank-0407@users.noreply.github.com>
2024-05-03 08:05:35 +0530

    Update Simplexmethod.py

--- Simplexmethod.py
+++ Simplexmethod.py
@@ -12,36 +12,36 @@ array2table(simplex_table,'VariableNames',var)
 
 RUN=true 
 while(RUN) 
-zc=zj_cj(1:end-1); % end gives last index of ele, we can also use length for this 
-if any(zc<0) % any is used to check if any 1 is <0 then go in loop 
-fprintf('The current BFS is not optimal') 
-[enter_var,piv_col]=min(zc); 
-pivot_col=A(:,piv_col); 
-sol=A(:,end); 
-if all(pivot_col<=0) 
-error('Unbounded Solution'); 
-else for i=1:size(A,1) 
-if(pivot_col(i)>0) 
-ratio(i)=sol(i)/pivot_col(i); 
-else ratio(i)=inf; 
-end 
-end 
-[leaving_var,pivot_row_index]=min(ratio); 
-pivot_row=A(pivot_row_index,:); 
-end 
-bv(pivot_row_index)=piv_col; %% changing basic variables for next table 
-pvt_element=A(pivot_row_index,piv_col) 
-A(pivot_row_index,:)=A(pivot_row_index,:)/pvt_element 
-for i=1:size(A,1) 
-if i~=pivot_row_index 
-A(i,:)=A(i,:)-A(i,piv_col)*A(pivot_row_index,:) % formula is new_value=old_value-corrpivotcolval*corrpivotrowval/pivotele 
-end 
-end 
-zj_cj=c(bv)*A-c; 
-next_table=[A;zj_cj]; 
-array2table(simplex_table,'VariableNames',var) 
-else 
-RUN=false 
-fprintf('The table is optimal') 
-end 
+  zc=zj_cj(1:end-1); % end gives last index of ele, we can also use length for this 
+  if any(zc<0) % any is used to check if any 1 is <0 then go in loop 
+    fprintf('The current BFS is not optimal') 
+    [enter_var,piv_col]=min(zc); 
+    pivot_col=A(:,piv_col); 
+    sol=A(:,end); 
+      if all(pivot_col<=0) 
+      error('Unbounded Solution'); 
+      else for i=1:size(A,1) 
+        if(pivot_col(i)>0) 
+        ratio(i)=sol(i)/pivot_col(i); 
+        else ratio(i)=inf; 
+        end 
+      end 
+    [leaving_var,pivot_row_index]=min(ratio); 
+    pivot_row=A(pivot_row_index,:); 
+    end 
+  bv(pivot_row_index)=piv_col; %% changing basic variables for next table 
+  pvt_element=A(pivot_row_index,piv_col) 
+  A(pivot_row_index,:)=A(pivot_row_index,:)/pvt_element 
+    for i=1:size(A,1) 
+      if i~=pivot_row_index 
+      A(i,:)=A(i,:)-A(i,piv_col)*A(pivot_row_index,:) % formula is new_value=old_value-corrpivotcolval*corrpivotrowval/pivotele 
+      end 
+    end 
+  zj_cj=c(bv)*A-c; 
+  next_table=[A;zj_cj]; 
+  array2table(simplex_table,'VariableNames',var) 
+  else 
+  RUN=false 
+  fprintf('The table is optimal') 
+  end 
 end
